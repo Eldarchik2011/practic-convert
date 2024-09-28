117b2d3667530ff0ed45bcf12fe660421b1a3dee
Eldarchik2011 <147970739+Eldarchik2011@users.noreply.github.com>
2024-09-28 11:07:24 +0500

    Add files via upload

---
+++ preo.js
@@ -0,0 +1,48 @@
+let x = 5;
+let y = 'hello';
+let z = true;
+let w = null;
+let v;
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+console.log(typeof(w));
+console.log(typeof(v));
+
+let str = "10";
+let str2 = "20.5";
+let str3 = "30px";
+
+console.log(parseInt(str));
+console.log(parseInt(str2));
+console.log(parseInt(str3));
+
+
+let str4 = "10.5";
+let str5 = "20";
+let str6 = "30.25px";
+
+console.log(parseFloat(str4));
+console.log(parseFloat(str5));
+console.log(parseFloat(str6));
+
+
+const userName = "John"
+const age = "16"
+
+
+console.log(`My name is ${userName} years old, i'm ${age}`);
+alert("Hello, i am learning JavaScript!");
+let num = 123;
+alert("123");
+
+let a = 6;
+alert("6");
+
+
+let amount = prompt('Напишите любое число', 50);
+let amount2 = prompt('Напишите еще одно любое число', 2);
+
+
+
+console.log("50" + "2");
